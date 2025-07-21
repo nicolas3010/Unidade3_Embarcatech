40ec51bd100f97a1291e79bd6fcc0175f04dbcfd
Nícolas de Carvalho <56662198+nicolas3010@users.noreply.github.com>
2025-07-21 14:12:07 -0300

    Update Luminosidade.c

--- Luminosidade/Luminosidade.c
+++ Luminosidade/Luminosidade.c
@@ -4,9 +4,7 @@
 #include "display.h"
 #include "bh1750.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+
 #define I2C_PORT i2c0
 #define I2C_SDA 0
 #define I2C_SCL 1
